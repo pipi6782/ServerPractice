245bab7b57b0c2f61cac9ebdb9433617eb286f60
pipi6782 <69996679+pipi6782@users.noreply.github.com>
2022-07-24 23:54:56 +0900

    2022-07-24
    13_LockBased_Stack_Queue 추가

---
+++ ServerPractice/12_TLS.cpp
@@ -0,0 +1,49 @@
+﻿/*
+	TLS는 Thread Local Storage 의 약자로 스레드마다 갖고 있는 로컬 저장소라고 볼 수 있다.
+	그런데 생각해보면 멀티스레드에서 각 스레드는 힙,데이터,코드영역을 공유하고 각각 별도의 스택영역을 갖는다.
+	그럼 스택영역과 TLS는 뭐가 다른걸까?
+	스택영역은 함수나 특정한 범위를 위한 공간이란것이 일반적이다.
+	그렇기 때문에 스택영역은 범위가 끝나면 스택프레임이 정리되면서 일정 부분은 사용할 수 없게 되는 불안정한 영역이라는 특징이 있다.
+	하지만 TLS는 스레드별로 독립적으로 주어지는 저장소로 전역공간처럼 쓸 수도 있다. (나만 쓸 수 있는 전역공간)
+*/
+
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <vector>
+
+using namespace std;
+
+//thread_local 키워드를 붙여주면 TLS구역에 변수가 할당된다.
+//기존 전역변수라면 다른 스레드가 값을 덮어 쓰는 순간 기존 스레드도 영향을 받지만
+//이 키워드가 있으면 영향을 받지 않는다.
+//C++ 11 부터는 thread_local 키워드로 사용하지만
+//이 전에는 __declspec(thread) 를 사용했다 했고 운영체제마다 방법이 달랐다 한다.
+
+thread_local int LThreadID = 0;
+
+void ThreadMain(int threadID)
+{
+	LThreadID = threadID;
+
+	while (true)
+	{
+		printf("스레드 %d\n",LThreadID);
+		this_thread::sleep_for(0.5s);
+	}
+}
+
+int main()
+{
+	vector<thread> threads;
+	for (int i = 0; i < 10; i++)
+	{
+		int threadID = i + 1;
+		threads.push_back(thread(ThreadMain, threadID));
+	}
+
+	for (thread& t : threads)
+	{
+		t.join();
+	}
+}
---
+++ ServerPractice/13_LockBased_Stack_Queue.cpp
@@ -0,0 +1,131 @@
+﻿/*
+	지금까지 했던 멀티스레드를 다시 한번 되짚어보는 느낌이다.
+	자료구조에 멀티스레드를 접목시켜보는 건데 이번엔 lock을 사용하는 자료구조이다.
+*/
+
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+template<typename T>
+class LockStack
+{
+public:
+	LockStack() = default;
+	LockStack(const LockStack&) = delete;
+	LockStack& operator=(const LockStack&) = delete;
+
+	void Push(T val)
+	{
+		std::lock_guard<std::mutex> lock(mutex);
+		stack.push(move(val));
+		cv.notify_one();
+	}
+
+	bool TryPop(T& val)
+	{
+		std::lock_guard<std::mutex> lock(mutex);
+		if (stack.empty())
+			return false;
+
+		val = move(stack.top());
+
+		stack.pop();
+		return true;
+	}
+
+	void WaitPop(T& val)
+	{
+		std::unique_lock<std::mutex> lock(mutex);
+		cv.wait(lock, [] {return stack.empty() == false; });
+		val = std::move(stack.top());
+		stack.pop();
+	}
+private:
+	stack<T> stack;
+	mutex mutex;
+	condition_variable cv;
+};
+
+template<typename T>
+class LockQueue
+{
+public:
+	LockQueue() = default;
+	LockQueue(const LockQueue&) = delete;
+	LockQueue& operator = (const LockQueue&) = delete;
+
+	void Push(T& val)
+	{
+		std::lock_guard<std::mutex> lock(mutex);
+
+		queue.push(val);
+
+		cv.notify_one();
+	}
+
+	bool TryPop(T& val)
+	{
+		std::lock_guard<std::mutex> lock(mutex);
+		if (queue.empty())
+			return false;
+
+		val = std::move(queue.front());
+
+		queue.pop();
+		return true;
+	}
+
+	void WaitPop(T& val)
+	{
+		std::unique_lock<std::mutex> lock(mutex);
+		cv.wait(lock, [] {return queue.empty() == false; });
+		val = std::move(queue.top());
+		queue.pop();
+	}
+
+private:
+	std::queue<T> queue;
+	std::mutex mutex;
+	std::condition_variable cv;
+};
+
+
+LockStack<int> s;
+LockQueue<int> q;
+
+void Push()
+{
+	while (true)
+	{
+		int data = rand()*rand();
+		q.Push(data);
+		this_thread::sleep_for(5ms);
+	}
+}
+
+
+void Pop()
+{
+	while (true)
+	{
+		int data = 0;
+		if (q.TryPop(data))
+		{
+			printf("%d\n", data);
+		}
+	}
+}
+
+int main()
+{
+	thread t1(Push);
+	thread t2(Pop);
+
+	t1.join();
+	t2.join();
+}
