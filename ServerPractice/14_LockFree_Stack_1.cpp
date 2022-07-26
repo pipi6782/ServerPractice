ca82ca34ace26e99030b22e80385bb046170ab8b
pipi6782 <69996679+pipi6782@users.noreply.github.com>
2022-07-27 02:43:42 +0900

    2022-07-27
    14_LockFree_Stack_1 추가
    15_LockFree_Stack_2 추가

---
+++ ServerPractice/14_LockFree_Stack_1.cpp
@@ -0,0 +1,192 @@
+﻿/*
+	여태껏 멀티스레드 프로그래밍을 할 땐 동기화를 위해 lock을 걸어주는 방식이 일반적이었다.
+	하지만 이번엔 lock을 사용하지 않으면서 동기화를 시켜줄 것이다.
+	Lock Free 방식이 lock을 사용하지 않기 때문에 성능이 더 좋아 보이지만
+	동기화를 위해서 많은 작업들이 필요하고 그러다보니 막상 성능은
+	lock을 쓴 것 만도 못한 상황이 생길때가 있다.
+*/
+
+#include <iostream>
+#include <thread>
+#include <stack>
+#include <queue>
+#include <atomic>
+
+using namespace std;
+
+template<typename T>
+class LockFreeStack
+{
+	struct Node
+	{
+		Node(const T& val) : Data(val), Next(nullptr) {}
+
+		T Data;
+		Node* Next;
+	};
+
+public:
+	void Push(const T& value)
+	{
+		Node* node = new Node(value);
+		node->Next = head;
+		while (head.compare_exchange_weak(node->Next, node) == false)
+		{
+			/*
+				근데 이건 스핀락과 유사하다고 봐도 되는것 아닌가?
+				Lock Free 프로그래밍이 말 그대로 mutex.lock() 을 안써서 Lock Free인가?
+			*/
+			/*
+				인프런 루키스님 답변
+			
+				Lock-Free는 Lock을 명시적으로 사용하지 않는다는 것은 맞지만
+				어차피 쓰레드 경합 방지 차원에서 CAS 류의 코드는 들어가야 합니다.
+
+				LockFreeQueue의 경우 데이터가 충분히 있다면 Push/Pop이 서로 영향을 주지 않으므로
+				Lock을 잡는 방식에 비해 조금 이점이 있을 수는 있긴 합니다.
+				그러나 서버에서 [우리는 LockFree 구조!!]라고 자랑한다고
+				그게 정말 효율적이라는 보장은 없고 오히려 Lock을 잡는게 좋을 수도 있습니다.
+				LockFree 외에도 WaitFree라는 용어도 종종 등장하는데
+				무한 뺑뺑이를 돌면서 CAS를 하지 않는 상황을 얘기합니다.
+				
+				마지막으로 Lock을 구현하는 여러가지 방식 중에 스핀락도 포함이 되는 것이라
+				[스핀락은 Lock이 아닌가?]는 맞지 않습니다.
+
+			*/
+		}
+	}
+
+	bool TryPop(T& value)
+	{
+		popCount++;
+		Node* oldHead = head;
+		while (oldHead && head.compare_exchange_weak(oldHead, oldHead->Next) == false)
+		{
+
+		}
+
+		if (oldHead == nullptr)
+		{
+			popCount--;
+			return false;
+		}
+
+		value = oldHead->Data;
+		TryDelete(oldHead);
+		return true;
+	}
+
+	void TryDelete(Node* oldHead)
+	{
+		//나 외에 누가 있는가?
+		if (popCount == 1)
+		{
+			//사용중인 스레드가 자기 자신뿐
+
+			//혼자인 김에 삭제 예약된 다른 데이터도 삭제
+			//1. 데이터를 분리하고
+			Node* node = pendingList.exchange(nullptr);
+			//2. 카운트를 체크하고
+			if (--popCount == 0)
+			{
+				//중간에 끼어든 스레드가 없다.
+				//3. 이제서야 삭제한다.
+				DeleteNodes(node);
+			}
+			else if(node != nullptr)
+			{
+				//다른 스레드가 껴들었기 때문에 원복
+				ChainPendingNodeList(node);
+			}
+
+			delete oldHead;
+		}
+		else
+		{
+			//다른 스레드도 이 노드를 사용중
+			ChainPendingNode(oldHead);
+		}
+	}
+
+	void ChainPendingNodeList(Node* first, Node* last)
+	{
+		last->Next = pendingList;
+		//멀티스레드이기 때문에 CAS알고리즘으로 계속 비교를 해야한다.
+		while (pendingList.compare_exchange_weak(last->Next, first) == false)
+		{}
+	}
+
+	void ChainPendingNodeList(Node* node)
+	{
+		Node* lastNode = node;
+		while (lastNode->Next)
+			lastNode = lastNode->Next;
+
+		ChainPendingNodeList(node, lastNode);
+	}
+
+	void ChainPendingNode(Node* node)
+	{
+		ChainPendingNodeList(node, node);
+	}
+
+	void DeleteNodes(Node* node)
+	{
+		while (node)
+		{
+			Node* next = node->Next;
+			delete node;
+			node = next;
+		}
+	}
+
+private:
+	atomic<Node*> head;
+	/*
+		Pop에서 그냥 멋도 모르고 삭제를 하면 while문에서 CAS알고리즘으로 검사를 할 때
+		삭제한 포인터를 쓰려다가 프로그램이 터질 수도 있다.
+		lock free는 요즘까지도 계속 연구되고 특허까지 나오고 있는 분야인 만큼 방식들이 계속 나오고 있는데
+		그 중 하나는 shared_ptr의 ref count를 비슷하게 따라하는 것 이다.
+	*/
+	//1. Pop을 실행중인 쓰레드 갯수를 체크
+	atomic<int> popCount = 0;	//Pop을 실행중인 쓰레드 갯수
+	atomic<Node*> pendingList;	//삭제되어야 할 노드들(제일 첫번째)
+};
+
+LockFreeStack<int> s;
+
+void Push()
+{
+	while (true)
+	{
+		int data = rand() * rand();
+		s.Push(data);
+		this_thread::sleep_for(1ms);
+	}
+}
+
+
+void Pop()
+{
+	while (true)
+	{
+		int data = 0;
+		if (s.TryPop(data))
+		{
+			printf("%d\n", data);
+		}
+	}
+}
+
+int main()
+{
+	thread t1(Push);
+	thread t2(Push);
+	thread t3(Pop);
+	thread t4(Pop);
+
+	t1.join();
+	t2.join();
+	t3.join();
+	t4.join();
+}
---
+++ ServerPractice/15_LockFree_Stack_2.cpp
@@ -0,0 +1,216 @@
+﻿#include <iostream>
+#include <thread>
+#include <stack>
+#include <queue>
+#include <atomic>
+
+using namespace std;
+
+//template<typename T>
+//class LockFreeStack
+//{
+//	struct Node
+//	{
+//		Node(const T& val) : Data(make_shared<T>(val)), Next(nullptr) {}
+//
+//		shared_ptr<T> Data;
+//		shared_ptr<Node> Next;
+//	};
+//
+//public:
+//	void Push(const T& value)
+//	{
+//		shared_ptr<Node> node = make_shared<Node>(value);
+//		node->Next = head;
+//
+//		//이제 head가 스마트 포인터로 바꼈다. 그럼 이제 CAS를 쓰지 못하는건가? 싶을 수도 있다.
+//		//하지만 사용 가능하다. atomic_compare_exchange_weak 와 같은 함수를 사용하면 된다.
+//		while (atomic_compare_exchange_weak(&head, &node->Next, node) == false)
+//		{
+//
+//		}
+//	}
+//
+//	shared_ptr<T> TryPop(T& value)
+//	{
+//		//shared_ptr을 그냥 바로 가져오는 것 역시 원자적이지 않다.
+//		//reference count가 영향을 받을 수 있기 때문.
+//		//이 역시 atomic_load 함수를 이용해서 받아온다.
+//		shared_ptr<Node> oldHead = atomic_load(&head);
+//
+//		while (oldHead && atomic_compare_exchange_weak(&head, &oldHead, oldHead->Next) == false)
+//		{
+//		}
+//
+//		if (oldHead == nullptr)
+//			return shared_ptr<T>();
+//
+//		return oldHead->Data;
+//	}
+//
+//
+//
+//private:
+//	/*
+//		Pop에서 그냥 멋도 모르고 삭제를 하면 while문에서 CAS알고리즘으로 검사를 할 때
+//		삭제한 포인터를 쓰려다가 프로그램이 터질 수도 있다.
+//		lock free는 요즘까지도 계속 연구되고 특허까지 나오고 있는 분야인 만큼 방식들이 계속 나오고 있는데
+//		이번 방법은 C#,JAVA의 GC의 느낌처럼 스마트포인터를 사용하는 것이다.
+//	*/
+//	shared_ptr<Node> head;
+//};
+
+/*
+	이 코드의 아주 치명적인 문제가 있다.
+	문제는 바로 이 코드가 lock free 방식이 아니라는 것이다.
+	그 이유는 shared_ptr 이 lock free로 동작하지 않기 때문
+	지금부터는 그런 문제들을 해결한 코드를 새로 만든다.
+	shared_ptr의 레퍼런스 카운팅을 직접 구현하는 방식이다.
+*/
+
+//물론 이 방식이 lock free라고 단정지을 수는 없다.
+//사용자의 pc 사양에 따라서 lock free여부가 바뀌기 때문.
+//일단은 "이건 lock free방식이다." 라는 전제하에 진행한다.
+template<typename T>
+class LockFreeStack
+{
+	struct CountedNodePtr;
+
+	struct Node
+	{
+		Node(const T& val) : Data(make_shared<T>(val)) {}
+
+		shared_ptr<T> Data;
+		CountedNodePtr Next;
+		atomic<int> internalCount = 0;
+	};
+
+	struct CountedNodePtr
+	{
+		int externalCount = 0;
+		Node* node = 0;
+	};
+
+public:
+	void Push(const T& value)
+	{
+		CountedNodePtr node;
+		node.node = new Node(value);
+		node.externalCount = 1;
+		node.node->Next = head;
+		//여기까지는 아무 문제 없다.
+		//스택 영역에서만 다루기 때문.
+		while (head.compare_exchange_weak(node.node->Next, node) == false)	//경합을 통해서 Push를 하겠다.
+		{}
+
+	}
+
+	shared_ptr<T> TryPop()
+	{
+		//기존 헤드를 가져온다. 근데 이 상태로는 온전하게 사용할 수 있다는 보장이 없다.
+		CountedNodePtr oldHead = head;
+		while (true)
+		{
+			//참조할 수 있는 권한을 획득한다. externalCount가 현 시점에서 1만큼 커진 값을 가진 애가 이긴다.
+			//Increase만 하고 Decrease는 하지 않는다.
+			//은행에서 번호표를 뽑는 느낌
+			IncreaseHeadCount(oldHead);
+			//지금 상태는 externalCount가 최소 2이상일꺼니 삭제하지 않는다.
+
+			Node* ptr = oldHead.node;
+			if (ptr == nullptr)
+				return shared_ptr<T>();
+
+			//소유권 획득 (ptr->Next 로 head를 바꾼 애가 이긴다.)
+			if (head.compare_exchange_strong(oldHead, ptr->Next))
+			{
+				//이곳에 들어왔단 것은 head와 oldHead가 완전히 일치한다는 것을 의미함
+				//여기 들어온 순간 head의 값은 바뀌기 때문에 다른 스레드가 이 조건문 전 까지 왔다 하더라도 head와 oldHead는 다른 상황임
+				shared_ptr<T> result;
+				result.swap(ptr->Data);
+
+				//external은 늘어나기만 하고 internal은 줄이기만 할거다.
+
+				//나 말고 다른 스레드가 쓰고 있는지 확인
+				const int countIncrease = oldHead.externalCount - 2;
+				if (ptr->internalCount.fetch_add(countIncrease) == -countIncrease)
+					delete ptr;
+
+				return result;
+			}
+			else if(ptr->internalCount.fetch_sub(1) == 1)
+			{
+				//참조는 할 수 있지만 소유는 할 수 없다.
+				//fetch_sub로 얻은 빼기 이전의 값이 1이면
+				//나만 남은 것이기 때문에 내가 삭제한다.
+				delete ptr;
+			}
+		}
+	}
+
+private:
+	void IncreaseHeadCount(CountedNodePtr& oldCounter)
+	{
+		while (true)
+		{
+			//newCounter은 우리가 원하는 externalCount가 딱 1만 늘어난 상태를 의미함
+			CountedNodePtr newCounter = oldCounter;
+			newCounter.externalCount++;
+			//그 상태에서 계속 CAS알고리즘으로 비교를 한 후 조건에 부합하면 반복문 탈출
+			if (head.compare_exchange_strong(oldCounter, newCounter))
+			{
+				//반복문 조건에 부합하니 지금은 안전하게 Count를 수정할 수 있다.
+				oldCounter.externalCount = newCounter.externalCount;
+				break;
+			}
+		}
+	}
+
+
+
+private:
+	atomic<CountedNodePtr> head;
+};
+
+LockFreeStack<int> s;
+
+void Push()
+{
+	while (true)
+	{
+		int data = rand() * rand();
+		s.Push(data);
+		this_thread::sleep_for(1ms);
+	}
+}
+
+
+void Pop()
+{
+	while (true)
+	{
+		auto data = s.TryPop();
+		if(data != nullptr)
+			printf("%d\n", *data);
+		
+	}
+}
+
+/*
+	lock free 프로그래밍은 상당히 어려운 분야다.
+	생각대로 해서 절대 안되고 로직에 구멍이있는지도 철저하게 확인해야한다.
+*/
+
+
+int main()
+{
+	thread t1(Push);
+	thread t2(Push);
+	thread t3(Pop);
+	thread t4(Pop);
+
+	t1.join();
+	t2.join();
+	t3.join();
+	t4.join();
+}
